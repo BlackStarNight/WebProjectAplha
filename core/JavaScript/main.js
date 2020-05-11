cd4e8d0812f5041bd6221c12280969ef7e02cfe2
Artyom <artemshimon2000@gmail.com>
2020-05-11 18:01:15 +0300

    update files

---
+++ core/JavaScript/main.js
@@ -0,0 +1,17 @@
+/*const app = angular.module('app',[]);
+
+app.controller('myCtrl', function ($scope) {
+    $scope.hello = "hello world!";
+
+    $scope.getBookMark = function () {
+        return "My bookmark"
+    }
+
+});
+
+app.factory('myFactory', function () {
+    return{
+        hello: 'hello world'
+    }
+
+})*/
